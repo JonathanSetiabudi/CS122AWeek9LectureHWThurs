c188a72b380791e1817e54a637b007c41fe0c3b2
Jonathan Setiabudi <107577120+JonathanSetiabudi@users.noreply.github.com>
2026-05-30 19:44:30 +0000

    Potentially finished hw

---
+++ mkdir.c
@@ -0,0 +1,22 @@
+/**** Program C8.1: mkdir.c: run as a.out dirname ****/
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+    int r;
+    if (argc < 2) {
+        printf("Usage: %s dirname\n", argv[0]); 
+        exit(1);
+    }
+
+    for(int i = 1; i < argc; i++){
+        if ((r = mkdir(argv[i], 0755)) < 0) {
+            perror("mkdir"); // print error message
+        }
+    }
+    
+    
+    return 0;
+}
